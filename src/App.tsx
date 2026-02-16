06b266fac8fa18f3d53c23aaec6d703406fd1e7c
Logan White <lwhite4965@outlook.com>
2026-02-16 14:15:25 -0500

    Most basic boilerplate for Clerk imaginable.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import GameBoard from "./GameBoard/GameBoard";
+import { ClerkProvider } from "@clerk/clerk-react";
 import "./App.css";
 
+// Grab pub Clerk key
+const PUB_KEY_CLERK = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 function App() {
-	return <GameBoard />;
+	return (
+		<ClerkProvider publishableKey={PUB_KEY_CLERK}>
+			<GameBoard />
+		</ClerkProvider>
+	);
 }
 
 export default App;
